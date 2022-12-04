c6f64ecb463a9927e3abe3850c635bbf891a6f39
John Colella <colella.john@gmail.com>
2022-12-04 12:48:02 -0500

    Complete 2022 Day 4

--- 2022/main.go
+++ 2022/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
-	"main/day3"
+	"main/day4"
 )
 
 func main() {
 	// res, err := day1.New().Run()
 	// res, err := day2.New().Run()
-	res, err := day3.New().Run()
+	// res, err := day3.New().Run()
+	res, err := day4.New().Run()
 
 	if err != nil {
 		fmt.Printf("ERROR %v", err)
